91df13cf05e159009bfff12ea3958c7e2de366da
Sandeep kumar yadav <124300037+ysandeepkumar821@users.noreply.github.com>
2024-03-20 01:15:51 +0530

    assignment salutation

---
+++ index3.js
@@ -0,0 +1,12 @@
+// 03. Swap the values.
+
+function swap(a, b) {
+    [a, b] = [b, a];
+    return [a, b];
+  }
+  
+  let x = 1;
+  let y = 2;
+  [x, y] = swap(x, y);
+  console.log(x, y);
+  
---
+++ index5.js
@@ -0,0 +1,12 @@
+// 05. Min and max values.
+
+function maxMin(arr) {
+    return {
+      max: Math.max(...arr),
+      min: Math.min(...arr),
+    };
+  }
+  
+  const array = [3, 7, 2, 9, 4, 1];
+  console.log(maxMin(array));
+  
